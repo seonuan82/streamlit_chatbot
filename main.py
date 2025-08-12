042bc3189e8f7d769d5a0757d9982529885bee0c
Seonu An <seonuan82@gmail.com>
2025-08-13 01:12:08 +0900

    Update main.py

--- main.py
+++ main.py
@@ -45,13 +45,13 @@ if input:
     if input == 'password':
       st.session_state.version = 'Revised'
       try:
-          assistant_text = get_basic_response(input)
+          assistant_text = '새로운 버전의 답변이에요.' + get_revised_response(input)
       except TypeError:   # API 오류가 난 경우, default로 출력
           assistant_text = "질문을 잘 이해하지 못했어요. 다시 입력해 주세요."
     else:
       st.session_state.version = 'Default'
       try:
-          assistant_text = '새로운 버전의 답변이에요.' + get_revised_response(input)
+          assistant_text = get_basic_response(input)
       except TypeError:   # API 오류가 난 경우, default로 출력
           assistant_text = "질문을 잘 이해하지 못했어요. 다시 입력해 주세요."
 
